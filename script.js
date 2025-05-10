114feb3dd59380332646a9c08ba9a724fe228de5
YAYARIR <yarikos20140107@gmail.com>
2025-05-11 02:28:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function playMusic() {
-    musicControls.style.display = 'block';
-    musicControls.classList.remove('hidden'); // Убираем класс скрытия
-    audio.play().catch(function(error) {
-        console.error("Ошибка воспроизведения:", error);
-    });
-}
+
